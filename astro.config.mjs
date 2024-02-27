2024f17631730f26eec4539d9675602d931bc45b
misturatech-jeanpierre <jeanpjordans10@gmail.com>
2024-02-27 10:50:46 -0500

    chore(app): remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,10 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
   integrations: [
     mdx(),
     sitemap(),
-    tailwind(),
-    react(),
+    tailwind()
   ],
 });
